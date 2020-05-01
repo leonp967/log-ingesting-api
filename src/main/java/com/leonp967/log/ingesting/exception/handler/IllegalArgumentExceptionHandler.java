1e9e01690d58d5a5e2ae8de08139aec082fab88b
leonp967 <leonardoporto159ster@gmail.com>
2020-05-01 01:47:50 -0300

    Adicionado exception handler

---
+++ src/main/java/com/leonp967/log/ingesting/exception/handler/IllegalArgumentExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.leonp967.log.ingesting.exception.handler;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class IllegalArgumentExceptionHandler implements ExceptionMapper<IllegalArgumentException> {
+
+    @Override
+    public Response toResponse(IllegalArgumentException e) {
+        return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+    }
+}
